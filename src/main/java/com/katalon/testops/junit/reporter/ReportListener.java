db1e2b994ec5db593d809fcb02000338400c107c
Qui Le <qui.le@katalon.com>
2020-12-25 16:48:11 +0700

    update

--- src/main/java/com/katalon/testops/junit/reporter/ReportListener.java
+++ src/main/java/com/katalon/testops/junit/reporter/ReportListener.java
@@ -7,138 +7,102 @@ import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 import org.slf4j.Logger;
 
-import java.text.MessageFormat;
-import java.util.Objects;
-
 public class ReportListener extends RunListener {
 
     private static final Logger logger = LogHelper.getLogger();
 
-    private static final String TESTOPS_GENERAL_ERROR = "An error has occurred in TestOps's report listener.\n"
-            + "Step: {0} \n"
-            + "Message: {1}";
+    private static final String TESTOPS_GENERAL_ERROR = "An error has occurred in step: %s";
 
-    TestRunManager testRunManager = null;
+    private final TestRunManager testRunManager;
 
     public ReportListener() {
         super();
-        try {
-            testRunManager = new TestRunManager();
-        } catch (Exception e) {
-            logger.error(getErrorMessage("initializing", e));
-        }
+        testRunManager = new TestRunManager();
     }
 
     @Override
     public void testRunStarted(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testRunStarted(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage("testRunStarted", e));
+            logger.error(getErrorMessage("testRunStarted"), e);
         }
     }
 
     @Override
     public void testRunFinished(Result result) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testRunFinished(result);
         } catch (Exception e) {
-            logger.error(getErrorMessage("testRunFinished", e));
+            logger.error(getErrorMessage("testRunFinished"), e);
         }
     }
 
     @Override
     public void testSuiteStarted(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testSuiteStarted(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testSuiteStarted", description), e));
+            logger.error(getErrorMessage(getStepName("testSuiteStarted", description)), e);
         }
     }
 
     @Override
     public void testSuiteFinished(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testSuiteFinished(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testSuiteFinished", description), e));
+            logger.error(getErrorMessage(getStepName("testSuiteFinished", description)), e);
         }
     }
 
     @Override
     public void testStarted(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testStarted(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testStarted", description), e));
+            logger.error(getErrorMessage(getStepName("testStarted", description)), e);
         }
     }
 
     @Override
     public void testFinished(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testFinished(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testFinished", description), e));
+            logger.error(getErrorMessage(getStepName("testFinished", description)), e);
         }
     }
 
     @Override
     public void testFailure(Failure failure) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testFailure(failure);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testFailure", failure.getDescription()), e));
+            logger.error(getErrorMessage(getStepName("testFailure", failure.getDescription())), e);
         }
     }
 
     @Override
     public void testAssumptionFailure(Failure failure) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testAssumptionFailure(failure);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testAssumptionFailure", failure.getDescription()), e));
+            logger.error(getErrorMessage(getStepName("testAssumptionFailure", failure.getDescription())), e);
         }
     }
 
     @Override
     public void testIgnored(Description description) {
-        if (Objects.isNull(testRunManager)) {
-            return;
-        }
         try {
             testRunManager.testIgnored(description);
         } catch (Exception e) {
-            logger.error(getErrorMessage(getStepName("testIgnored", description), e));
+            logger.error(getErrorMessage(getStepName("testIgnored", description)), e);
         }
     }
 
-    private String getErrorMessage(String step, Exception e) {
-        return MessageFormat.format(TESTOPS_GENERAL_ERROR, step, e.getMessage());
+    private String getErrorMessage(String step) {
+        return String.format(TESTOPS_GENERAL_ERROR, step);
     }
 
     private String getStepName(String name, Description description) {
---
+++ src/test/java/com/katalon/testops/junit/ParamsTest.java
@@ -0,0 +1,36 @@
+package com.katalon.testops.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ParamsTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }
+        });
+    }
+
+    private int fInput;
+
+    private int fExpected;
+
+    public ParamsTest(int input, int expected) {
+        this.fInput = input;
+        this.fExpected = expected;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(fExpected, fInput);
+    }
+}
